4f128671dd11080239ef11410cbc38c0eb40d734
EmiliyanG6 <emiliyan.geshkov.a65@learn.telerikacademy.com>
2024-11-21 15:10:06 +0200

    Base Program

---
+++ main/src/main/java/com/example/demo/config/HibernateConfig.java
@@ -0,0 +1,55 @@
+package com.example.demo.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
+public class HibernateConfig {
+
+    private final String dbUrl, dbUsername,dbPassword;
+
+    @Autowired
+    public HibernateConfig(Environment env) {
+        dbUrl = env.getProperty("database.url");
+        dbUsername = env.getProperty("database.username");
+        dbPassword = env.getProperty("database.password");
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean entityManagerFactory() {
+        LocalSessionFactoryBean entityManagerFactory = new LocalSessionFactoryBean();
+        entityManagerFactory().setDataSource(dataSource());
+        entityManagerFactory().setPackagesToScan("com.example.demo.models");
+        entityManagerFactory().setHibernateProperties(hibernateProperties());
+        return entityManagerFactory;
+    }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        hibernateProperties.setProperty("hibernate.format_sql", "true");
+        return hibernateProperties;
+    }
+}
